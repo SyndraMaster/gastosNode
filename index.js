6eed2c530d825d90062f3df9047a02a84b6d2184
SyndraMaster <jaycendy@outlook.com>
2023-01-27 10:55:12 -0500

    Feat: :sparckles: cambiado motor ejs

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
+const ejs = require('ejs')
 const bodyParser = require('body-parser');
 const mongoose = require ('mongoose');
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 app.get('/', (req,res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.render(__dirname + '/index');
 })
 app.listen(3000, () => {
     console.log('Tu server está listo para usarse');
