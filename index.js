98a1dc4dbe02b467ae1c61871b6470c890dfb8ae
SyndraMaster <jaycendy@outlook.com>
2023-02-10 16:43:52 -0500

    Feat: 🧐 Pendiente inspeccion de base de datos * se necesita revisar cuando la base de datos está vacía.

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require('express');
 const app = express();
 const ejs = require('ejs')
 const bodyParser = require('body-parser');
-const mongoose = require ('mongoose');
+const mongoose = require('mongoose');
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
+let total = 0;
 
 main().catch(err => console.log(err));
 
@@ -17,25 +18,55 @@ async function main() {
     valor: Number,
     categoria: String,
     tipo: String,
-    fecha: String
+    fecha: Date
   });
   const Transacciones = mongoose.model('transaccion', transaccionesSchema);
-  const transaccion = new Transacciones ({
+  const transaccion = new Transacciones({
     nombre: 'Arriendo',
     valor: 420000,
     categoria: 'Vivienda',
     tipo: 'gasto',
-    fecha: '2023-01-30'
-  })
+    fecha: {
+      type: Date,
+      default: Date.now
+    }
+  });
   // transaccion.save();
-  const consulta = await Transacciones.find({nombre: 'Arriendo'});
+  const consulta = await Transacciones.find({ nombre: 'Arriendo' });
   console.log(consulta);
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+  app.post('/', (req, res) => {
+    let descripcion = req.body.descripcion;
+    let valorFormulario = req.body.valor;
+    let categoriaFormulario = req.body.categoria;
+    let tipoFormulario = req.body.tipo;
+    const nuevaEntrada = new Transacciones({
+      nombre: descripcion,
+      valor: valorFormulario,
+      categoria: categoriaFormulario,
+      tipo: tipoFormulario
+    })
+    nuevaEntrada.save();
+    
+    // res.redirect('/');
+    if (total[0].total > 0) {
+      res.render(__dirname + '/index', {valor: total[0].total});
+    }
+  });
+  total = await Transacciones.aggregate([{
+    $group: {
+      _id: null,
+      total: { $sum: "$valor" }
+    }
+  }]);
 }
-app.get('/', (req,res) => {
-    res.render(__dirname + '/index');
+app.get('/', (req, res) => {
+  if (total[0].total != []) {
+    res.render(__dirname + '/index', {valor: total[0].total});
+  } else {
+    res.render(__dirname + '/index', {valor: 0});
+  }
 })
 
 app.listen(3000, () => {
-    console.log('Tu server está listo para usarse en el puerto 3000');
+  console.log('Tu server está listo para usarse en el puerto 3000');
 })
--- public/js/main.js
+++ public/js/main.js
@@ -3,15 +3,18 @@ let agregar = document.querySelector('.agregar');
 let quitar = document.querySelector('.disminuir');
 let modal = document.querySelector('.formulario-modal');
 let cerrar = document.querySelector('.cerrar');
+let tipo = document.querySelector('.tipo');
 cerrar.addEventListener('click', () => {
     modal.style.display = 'none';
 })
 agregar.addEventListener('click', () => {
     titulo.textContent = 'Agregar Ingreso';
     modal.style.display = 'flex';
+    tipo.setAttribute('value', 'ingreso');
 })
 quitar.addEventListener('click', () => {
     titulo.textContent = 'Agregar Gasto';
     modal.style.display = 'flex';
+    tipo.setAttribute('value', 'gasto');
 })
 console.log(titulo)
