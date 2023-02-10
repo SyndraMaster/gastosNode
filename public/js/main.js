78c6dfac010dcbc1a43b592271b0d613e22bb44a
SyndraMaster <jaycendy@outlook.com>
2023-02-10 15:32:44 -0500

    feat: 💄Estilizado de elementos de formulario

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,17 @@
 let titulo = document.querySelector('.titulo-modal');
+let agregar = document.querySelector('.agregar');
+let quitar = document.querySelector('.disminuir');
+let modal = document.querySelector('.formulario-modal');
+let cerrar = document.querySelector('.cerrar');
+cerrar.addEventListener('click', () => {
+    modal.style.display = 'none';
+})
+agregar.addEventListener('click', () => {
+    titulo.textContent = 'Agregar Ingreso';
+    modal.style.display = 'flex';
+})
+quitar.addEventListener('click', () => {
+    titulo.textContent = 'Agregar Gasto';
+    modal.style.display = 'flex';
+})
+console.log(titulo)
